cecea84364dede813a275ff116e34b36d804206a
TianjianNi <37985120+TianjianNi@users.noreply.github.com>
2023-12-15 22:21:25 -0500

    update model.py
    Change the default num_classes to 10.

--- model.py
+++ model.py
@@ -345,8 +345,10 @@ class Block(nn.Module):
         return x
 
 
+# In official implementation, default num_classes was 1000
 class InceptionTransformer(nn.Module):
-    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=1000, embed_dims=None, depths=None,
+    def __init__(self, img_size=224, patch_size=16, in_chans=3, num_classes=10,
+                 embed_dims=None, depths=None,
                  num_heads=None, mlp_ratio=4., qkv_bias=True,
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0., embed_layer=PatchEmbed, norm_layer=None,
                  act_layer=None, weight_init='',
